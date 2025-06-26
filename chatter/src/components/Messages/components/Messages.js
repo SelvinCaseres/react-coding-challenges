67be404765c09f97dc586796b2829d383b9b4c73
Selvin Caseres <selvin.caseres@yahoo.com>
2025-06-25 19:42:39 -0500

    Push

--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 import useSound from 'use-sound';
 import config from '../../../config';
@@ -8,6 +8,7 @@ import Header from './Header';
 import Footer from './Footer';
 import Message from './Message';
 import '../styles/_messages.scss';
+import initialBottyMessage from '../../../common/constants/initialBottyMessage';
 
 const socket = io(
   config.BOT_SERVER_ENDPOINT,
@@ -19,10 +20,64 @@ function Messages() {
   const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
   const { setLatestMessage } = useContext(LatestMessagesContext);
 
+  const [message, setMessage ] = useState();
+  const [messages, setMessages ] = useState([{message: initialBottyMessage, user: "bot", botTyping: false}]);
+  const [botTyping, setBotTyping] = useState(false);
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const sendMessage = async () => {
+    playSend();
+
+    setMessages((prev) => [...prev, {message: message, user: "me"}])
+    setLatestMessage("bot", message);
+    
+    socket.emit("user-message", message); 
+  }
+
+  useEffect(() => {
+    socket.on('bot-message', (botmsg) => {
+      
+    setBotTyping(false);
+    setMessages((prev) => [...prev, {message: botmsg, user: "bot"}])
+    setLatestMessage("bot", botmsg);
+    playReceive();
+    
+  });
+
+    return () => {
+    socket.off('bot-message');
+  };}, []);
+
+  useEffect(() => {
+    socket.on('bot-typing', () => {
+    setBotTyping(true);
+  });
+
+    return () => {
+    socket.off('bot-typing');
+  };}, []);
+
+  const onChangeMessage = (input) =>{
+    setMessage(input);
+  }
+
+const messageListRef = useRef(null);
+
+  useEffect(() => {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [messages, botTyping]);
+
   return (
     <div className="messages">
       <Header />
-      <div className="messages__list" id="message-list">
+      <div className="messages__list" id="message-list" ref={messageListRef}>
+        {messages.map((msg, index) => (
+          <Message key={index} message={msg} nextMessage={messages[index + 1]} />
+        ))}
+        {botTyping ? <TypingMessage /> : null}
       </div>
       <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
     </div>
